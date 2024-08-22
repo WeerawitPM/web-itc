3595c6a4c28b3d618a0f7b5d6875c037a86e5152
Weerawit Phumphuang <weerawit.pm@gmail.com>
2024-08-22 11:56:07 +0700

    fix: header and card

--- web/src/app/company/[id]/page.tsx
+++ web/src/app/company/[id]/page.tsx
@@ -67,7 +67,7 @@ export default async function Home({ params }: Props) {
         <>
             <Header title={companyName} />
             <section id="services" className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 py-5">
-                <div className="flex flex-row flex-wrap justify-start gap-10">
+                <div className="flex flex-row flex-wrap justify-center gap-10">
                     {filteredData.map((website: {
                         id: string;
                         attributes: {
--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -2,13 +2,9 @@ import Link from "next/link";
 
 export default function Header({ title }: { title: string }) {
     return (
-        <header id="header" className="left-0 w-full flex items-center bg-gray-100 mt-[88px] border">
-            <div className="container mx-auto flex items-center justify-between px-4 py-4">
-
-                <div className="flex items-center">
-                    <h1 className="text-2xl font-bold text-red-700 my-auto">{title} - Website</h1>
-                </div>
-
+        <header id="header" className="w-full flex items-center bg-gray-100 mt-[88px] border">
+            <div className="container py-4">
+                <h1 className="text-2xl font-bold text-red-700 my-auto">{title} - Website</h1>
                 {/* <nav id="navmenu" className="hidden md:flex items-center space-x-4">
                     <ul className="flex space-x-4">
                         <li><a href="index.html#hero" className="text-blue-600 hover:text-blue-800">Home</a></li>
