3ecc6635d1dc5886a7529026a215f1954311636a
Weerawit Phumphuang <weerawit.pm@gmail.com>
2024-09-21 09:54:45 +0700

    fix: bug card and content not show

--- web/src/components/company/card.tsx
+++ web/src/components/company/card.tsx
@@ -9,28 +9,25 @@ type Props = {
     title: string;
     content: string;
     detail: React.FC<{ title: string; content: string }>; // Adjusted type
+    tutorial: string; //
 };
 
-export default function CardComponent({ href, src, title, content, detail: Detail }: Props) {
+export default function CardComponent({ href, src, title, content, detail: Detail, tutorial }: Props) {
     return (
-        <div className="d-flex text-center">
-            <Card className="w-[270px]" isPressable isHoverable radius="sm">
-                <CardBody >
-                    <div className="w-fit mx-auto my-auto">
-                        <Image src={src} width={150} height={150} unoptimized alt='image' className="object-cover w-[100%] h-[150px]" />
-                    </div>
+        <div className="d-flex justify-center">
+            <Card className="w-[270px] h-[350px]" isPressable isHoverable radius="sm">
+                <CardBody className="flex flex-col items-center justify-center">
+                    <Image src={src} width={150} height={150} unoptimized alt='image' className="object-cover w-[150px] h-[150px]" />
                 </CardBody>
-                <CardFooter>
-                    <div className="flex flex-col h-16">
-                        <div className="text-start font-bold">{title}</div>
-                        <div className="flex gap-2 justify-center align-middle mt-2">
-                            <Link href={href} target="_blank"><Button size="sm" radius="lg" className="accent-color text-white">Visit</Button></Link>
-                            <Detail title={title} content={content} />
-                            <Button size="sm" radius="lg" disabled>Tutorial</Button>
-                        </div>
+                <CardFooter className="flex flex-col items-center justify-between">
+                    <div className="text-start font-bold">{title}</div>
+                    <div className="flex gap-2 justify-center mt-2">
+                        <Link href={href} target="_blank"><Button size="sm" radius="lg" className="accent-color text-white">Visit</Button></Link>
+                        <Detail title={title} content={content} />
+                        <Link href={tutorial} target="_blank"><Button size="sm" radius="lg"  className="accent-color text-white">Tutorial</Button></Link>
                     </div>
                 </CardFooter>
             </Card>
         </div>
-    )
+    );
 }
