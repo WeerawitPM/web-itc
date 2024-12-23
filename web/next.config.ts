19816db80a53c305c7a09891a4a83b77e0109b86
Weerawit Phumphuang <weerawit.pm@gmail.com>
2024-12-23 16:41:55 +0700

    update: show image all domain

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
     domains: ["teaminsights.io", "localhost", "192.168.20.16"],
   },
 };
